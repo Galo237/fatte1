b70ff78e640f5cebad2776f2c356e91f9e10a198
Giarettamat <giaretta.mat@gmail.com>
2024-04-16 01:32:42 -0300

    up

---
+++ application/views/console/pages/sobre.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fatte</title>
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+<header>
+        <div class="header">
+            <div class="logo">Fatte</div>
+                <nav class="menu">
+                    <ul>
+                        <li>Destaque</li>
+                        <li>Masculino</li>
+                        <li>Feminino</li>
+                        <li>Sobre</li>
+                    </ul>
+                </nav>
+            <div class="icons">
+                <span class="search-icon"><img src="../imagens/search.png" alt=""></span>
+                <span class="cart-icon"><img src="../imagens/cart.png" alt=""></span>
+                <a href="pages/cadastro.php"><span class="profile-icon"><img src="../imagens/user.png" alt=""></span></a>
+            </div> 
+        </div> 
+    </header>
+
+    
+
+    <footer>
+        <div class="footer-left">
+            <h3>CONTATO</h3>
+            <p><b>Email: </b>fattecommerce@gmail.com</p>
+            <p><b>Telefone: </b>+55 41 99842-8507</p>
+            <br>
+            <p>Segunda à sexta: 09:00 às 18:00</p>
+            <p>Sábado: 09:00 às 15:00</p>
+            <p>Exceto Feriados</p>
+        </div>
+        <div class="footer-right">
+            <h3>REDES SOCIAIS</h3>
+            <a class="instagram"></a><img src="imagens/instagram.png" alt=""><p>@fatteclothing</p>
+            <a class="twitter"><img src="imagens/twitter.png" alt=""></a><p>@fatteclothing</p>
+            <a class="facebook"><img src="imagens/facebook.png" alt=""></a><p>Fatte Clothing</p>
+        </div>
+    </footer>
+</body>
+</html>
