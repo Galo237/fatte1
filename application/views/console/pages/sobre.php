ac8ceb29f5ad6d88237a96dac7dd5f3398125023
Giarettamat <giaretta.mat@gmail.com>
2024-04-18 21:30:34 -0300

    up

--- application/views/console/pages/sobre.php
+++ application/views/console/pages/sobre.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fatte</title>
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="../css/sobre.css">
 </head>
 <body>
 <header>
@@ -24,11 +24,31 @@
                 <a href="pages/cadastro.php"><span class="profile-icon"><img src="../imagens/user.png" alt=""></span></a>
             </div> 
         </div> 
-    </header>
+</header>
+    <div class="sobre">
+        <span class="imgsobre"><img src="../imagens/sobre.jpg" alt=""></span>
+        <h1>Sobre nós</h1>
+        <p>No coração de uma cidade agitada, onde os sons das ruas se misturam com a energia pulsante da vida urbana, nasceu uma ideia que iria mudar o mundo da moda para sempre. Em um pequeno estúdio, um grupo de visionários se reuniu com um propósito: criar uma marca que não apenas vestisse corpos, mas também contasse histórias. Assim nasceu "Fatte".
+
+O nome "Fatte" é uma homenagem à ousadia e à coragem. Derivado da palavra italiana "Fatto", que significa "feito", a marca se compromete a criar peças que são mais do que simples roupas; são manifestações de estilo, individualidade e autoexpressão. <br><br>
+
+Desde o seu início modesto, a missão da "Fatte" tem sido clara: desafiar o convencional, abraçar a diversidade e celebrar a autenticidade. Cada coleção é cuidadosamente projetada para capturar a essência da vida moderna, combinando estilos clássicos com uma abordagem contemporânea.
+
+Na "Fatte", acreditamos que a moda vai além das tendências passageiras; é uma forma de arte que transcende o tempo. Por isso, nossas peças são criadas com atenção meticulosa aos detalhes, utilizando materiais de alta qualidade e técnicas de fabricação inovadoras. <br><br>
+
+Mas nossa dedicação vai além de simplesmente criar roupas. A "Fatte" está comprometida com a sustentabilidade e a responsabilidade social. Buscamos constantemente maneiras de minimizar nosso impacto no meio ambiente, desde a escolha de materiais eco-friendly até a implementação de práticas éticas em toda a cadeia de produção. <br><br>
+
+À medida que continuamos a crescer e evoluir, mantemos firmemente nossos valores fundamentais. Na "Fatte", acreditamos que todos têm o direito de se expressar livremente, sem medo de julgamento ou restrição. Somos uma comunidade diversificada de indivíduos apaixonados, unidos pela nossa paixão pela moda e pelo desejo de fazer a diferença no mundo.
+
+Junte-se a nós em nossa jornada enquanto continuamos a redefinir os limites da moda e inspirar uma nova geração de criatividade e autoexpressão. Esta é a história da "Fatte" - uma marca que não apenas veste corpos, mas também alimenta almas. Seja ousado. Seja autêntico. Seja "Fatte".</p>
+
 
     
+    </div>
 
-    <footer>
+
+
+<footer>
         <div class="footer-left">
             <h3>CONTATO</h3>
             <p><b>Email: </b>fattecommerce@gmail.com</p>
@@ -44,6 +64,6 @@
             <a class="twitter"><img src="imagens/twitter.png" alt=""></a><p>@fatteclothing</p>
             <a class="facebook"><img src="imagens/facebook.png" alt=""></a><p>Fatte Clothing</p>
         </div>
-    </footer>
+</footer>
 </body>
 </html>
