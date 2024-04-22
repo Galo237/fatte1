afad1cb9d743118ce69bb337695c31cf978c1c2d
rafaelGdF <rafaelgfreitas014@gmail.com>
2024-04-22 14:14:19 -0300

    up (masculino)

--- application/views/console/pages/masculino.php
+++ application/views/console/pages/masculino.php
@@ -7,25 +7,114 @@
     <title>Masculino</title>
 </head>
 <body>
-<header>
-        <nav>
-            <div class="header-left">
-                <h1 class="button">Fatte</h1>
+    <div class="header">
+            <div class="logo"><a href="../index.php">Fatte</a></div>
+            <nav class="menu">
+                <ul>
+                    <a href=""><li class="btn">Destaque</li></a>
+                    <a href="pages/masculino.php"><li class="btn">Masculino</li></a>
+                    <a href="pages/feminino.php"><li class="btn">Feminino</li></a>
+                    <a href="pages/sobre.php"><li class="btn">Sobre</li></a>
+                </ul>
+            </nav>
+        <div class="icons">
+            <span class="search-icon"><img src="../imagens/search1.png" alt=""></span>
+            <a href="../controllers/dashboard.php"><span class="cart-icon"><img src="../imagens/cart1.png" alt=""></span></a>
+            <a href="pages/cadastro.php"><span class="profile-icon"><img src="../imagens/user1.png" alt=""></span></a>
+        </div> 
+    </div>
+    <div class="page-inner-content">
+            <div class="product">
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
+                <div class="roupa">
+                    <img src="../imagens/roupa1.jpg" alt="">
+                    <p>Camiseta Fatte</p>
+                    <p>R$140.00</p>
+                </div>
             </div>
-            <div class="header-center">
-                <h2>
-                <a href="pages/masculino.php" class="btn"><b>Masculino</b></a>
-                <a href="index.php" class="btn"><b>Destaques</b></a>
-                <a href="pages/feminino.php" class="btn"><b>Feminino</b></a>
-                <a href="pages/sobre.php" class="btn"><b>Sobre</b></a>
-                </h2>
+    </div>
+
+    <footer>
+        <div class="footer-left">
+            <h3>CONTATO</h3>
+            <p><b>Email: </b>fattecommerce@gmail.com</p>
+            <p><b>Telefone: </b>+55 41 99842-8507</p>
+            <br>
+            <p>Segunda à sexta: 09:00 às 18:00</p>
+            <p>Sábado: 09:00 às 15:00</p>
+            <p>Exceto Feriados</p>
+        </div>
+        <div class="footer-right">
+            <h3>REDES SOCIAIS</h3>
+            <div class="instagram">
+                <a href="https://www.instagram.com/fatteclothing?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank"><img src="../imagens/instagram.png" alt=""></a>
+                <p>@fatteclothing</p>
             </div>
-            <div class="header-right">
-                <span class="search-icon"></span>
-                <span class="cart-icon"></span>
-                <span class="user-icon"></span>               
+            <div class="twitter">
+                <a href=""><img src="../imagens/twitter.png" alt=""></a>
+                <p>@fatteclothing</p>
             </div>
-        </nav>
-    </header>
+            <div class="facebook">
+                <a><img src="../imagens/facebook.png" alt=""></a>
+                <p>Fatte Clothing</p>
+            </div>
+        </div>       
+    </footer>
+
+    <script src="../js/script.js"></script>
 </body>
 </html>
