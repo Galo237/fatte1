d83024ce15609223694ae0b0d109b4e9c56ebd7d
rafaelGdF <rafaelgfreitas014@gmail.com>
2024-05-23 11:36:35 -0300

    Pesquisa de produtos

--- application/views/console/pages/masculino.php
+++ application/views/console/pages/masculino.php
@@ -4,7 +4,8 @@ session_start();
 
 include '../../../controllers/connection.php';
 
-$sql = "SELECT * FROM produtos WHERE proGenero = 'M'";
+$search = isset($_POST['search']) ? $_POST['search'] : '';
+$sql = "SELECT * FROM produtos WHERE proGenero = 'M' AND proNome LIKE '%$search%'";
 $result = $conn->query($sql);
 
 if(isset($_GET['logout'])) {
@@ -45,6 +46,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/masculino.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <title>Masculino</title>
     <style>
         /* Adicionar estilos para o modal */
@@ -81,9 +83,74 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
             text-decoration: none;
             cursor: pointer;
         }
+
+        .modal-content-custom {
+            background-color: #fefefe;
+            margin: 5% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 80%;
+            max-width: 600px;
+        }
+
+        .navbar-custom {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0.5rem 1rem;
+        }
+        .navbar-custom .navbar-nav {
+            flex-direction: row;
+        }
+        .navbar-custom .navbar-nav .nav-item {
+            margin-left: 1rem;
+        }
+        .navbar-custom .form-inline {
+            display: flex;
+            align-items: center;
+        }
+        .navbar-custom .form-inline input {
+            margin-right: 0.5rem;
+        }
+
+        .btn {
+            background-color: #ffffff;
+            color: #000000;
+            padding: 10px 20px;
+            border-radius: 5px;
+            text-decoration: none;
+            margin-right: 10px;
+            cursor: pointer;
+        }
+
+        footer {
+            background-color: #000000; /* Altera o fundo para preto */
+            color: #ffffff;
+            padding: 40px;
+        }
+
+        .instagram, .twitter, .facebook {
+            display: flex;
+            align-items: center;
+            margin-bottom: 10px;
+        }
+
+        .instagram p, .twitter p, .facebook p {
+            margin: 0;
+        }
+
+        .mr-2 {
+            margin-right: 0.5rem !important;
+        }
+
+        .mb-2 {
+            margin-bottom: 0.5rem !important;
+        }
+
+
     </style>
     <script>
-        
         function openModal(proId, proNome, proPreco, proImagem) {
             document.getElementById('modal').style.display = 'block';
             document.getElementById('modal-proId').value = proId;
@@ -98,34 +165,47 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
     </script>
 </head>
 <body>
-    <div class="header">
-        <div class="logo"><a href="../index.php">Fatte</a></div>
-        <nav id="nav">
-            <button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">
-                <span id="hamburger"></span>
-            </button>
-            <ul id="menu" role="menu">
-                <li><a href="">Destaque</a></li>
-                <li><a href="masculino.php">Masculino</a></li>
-                <li><a href="feminino.php">Feminino</a></li>
-                <li><a href="sobre.php">Sobre</a></li>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+        <a class="navbar-brand" href="../index.php">Fatte</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Destaque</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="masculino.php">Masculino</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="feminino.php">Feminino</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="sobre.php">Sobre</a>
+                </li>
             </ul>
-        </nav>
-        <div class="icons">
-            <span class="search-icon"><img src="../imagens/search1.png" alt=""></span>
-            <a href="carrinho.php"><span class="cart-icon"><img src="../imagens/cart1.png" alt=""></span></a>
-            <?php
-            if(isset($_SESSION['cliente'])) {
-                $nome_usuario = $_SESSION['nome'];
-                echo "<a href='perfil.php' style='text-decoration: none; color: black;'><span class='user-icon'>$nome_usuario</span></a>";
-                echo "<a href='?logout' class='logout'><img src='../imagens/logout.png' alt=''></a>";
-            } else {
-                echo "<a href='perfil.php'><span class='user-icon'><img src='../imagens/user1.png' alt=''></span></a>";
-            }
-            ?>
-        </div> 
-    </div>
-    <h3>Catálogo Masculino</h3>
+            <form class="form-inline my-2 my-lg-0" method="post" action="masculino.php">
+                <input class="form-control mr-sm-2" type="search" name="search" placeholder="Pesquisar produtos">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+            </form>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="carrinho.php"><img src="../imagens/cart1.png" alt="Cart"></a>
+                </li>
+                <?php
+                if(isset($_SESSION['cliente'])) {
+                    $nome_usuario = $_SESSION['nome'];
+                    echo "<li class='nav-item'><a class='nav-link' href='perfil.php'>$nome_usuario</a></li>";
+                    echo "<li class='nav-item'><a class='nav-link' href='?logout'><img src='../imagens/logout.png' alt='Logout'></a></li>";
+                } else {
+                    echo "<li class='nav-item'><a class='nav-link' href='perfil.php'><img src='../imagens/user1.png' alt='User'></a></li>";
+                }
+                ?>
+            </ul>
+        </div>
+    </nav>
+    <h3 style="margin-top: 50px; text-align: center">Catálogo Masculino</h3>
     <div class="page-inner-content">
         <div class="product" id="product">
             <?php
@@ -145,10 +225,11 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
         </div>
     </div>
 
+    <!-- Modal para Detalhes do Produto -->
     <div id="modal" class="modal">
-        <div class="modal-content">
+        <div class="modal-content-custom">
             <span class="close" onclick="closeModal()">&times;</span>
-            <img id="modal-proImagem" src="data:image/jpeg;base64,$encoded_image" alt="Image not loading!" style="width: 100%; max-width: 300px;">
+            <img id="modal-proImagem" src="" alt="Image not loading!" style="width: 100%; max-width: 300px;">
             <h2 id="modal-proNome"></h2>
             <p id="modal-proPreco"></p>
             <form method="post" action="masculino.php">
@@ -167,29 +248,44 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['add_to_cart'])) {
         </div>
     </div>
 
-    <footer>
-        <div class="footer-left">
-            <h3>CONTATO</h3>
-            <p><b>Email: </b>fattecommerce@gmail.com</p>
-            <p><b>Telefone: </b>+55 41 99842-8507</p>
-            <br>
-            <p>Segunda à sexta: 09:00 às 18:00</p>
-            <p>Sábado: 09:00 às 15:00</p>
-            <p>Exceto Feriados</p>
-        </div>
-        <div class="footer-right">
-            <h3>REDES SOCIAIS</h3>
-            <div class="instagram">
-                <a href="https://www.instagram.com/fatteclothing?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank"><img src="../imagens/instagram.png" alt=""></a>
-                <p>@fatteclothing</p>
-            </div>
-            <div class="twitter">
-                <a href=""><img src="../imagens/twitter.png" alt=""></a>
-                <p>@fatteclothing</p>
-            </div>
-            <div class="facebook">
-                <a><img src="../imagens/facebook.png" alt=""></a>
-                <p>Fatte Clothing</p>
+    <!-- Scripts do Bootstrap -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <footer class="bg-black text-white py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>CONTATO</h3>
+                    <p><b>Email: </b>fattecommerce@gmail.com</p>
+                    <p><b>Telefone: </b>+55 41 99842-8507</p>
+                    <br>
+                    <p>Segunda à sexta: 09:00 às 18:00</p>
+                    <p>Sábado: 09:00 às 15:00</p>
+                    <p>Exceto Feriados</p>
+                </div>
+                <div class="col-md-6">
+                    <h3>REDES SOCIAIS</h3>
+                    <div class="d-flex mb-2">
+                        <a href="https://www.instagram.com/fatteclothing?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">
+                            <img src="../imagens/instagram.png" alt="Instagram" class="mr-2">
+                        </a>
+                        <p>@fatteclothing</p>
+                    </div>
+                    <div class="d-flex mb-2">
+                        <a href="#">
+                            <img src="../imagens/twitter.png" alt="Twitter" class="mr-2">
+                        </a>
+                        <p>@fatteclothing</p>
+                    </div>
+                    <div class="d-flex">
+                        <a href="#">
+                            <img src="../imagens/facebook.png" alt="Facebook" class="mr-2">
+                        </a>
+                        <p>Fatte Clothing</p>
+                    </div>
+                </div>
             </div>
         </div>
     </footer>
